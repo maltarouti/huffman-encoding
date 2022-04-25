206b4eed1aa2da733f94726115d86f52909d9403
Murtada Altarouti <63660298+Murtada-Altarouti@users.noreply.github.com>
2022-04-25 23:37:31 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+#include "DynamicArray.h"
+#include "PriorityQueue.h"
+#include "HuffmanCoding.h"
+#include "test.h"
+
+
+int main(int argc, char *argv[]) {
+	encode("test.txt");
+	return 0;
+}
